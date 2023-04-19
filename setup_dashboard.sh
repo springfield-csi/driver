028a3304e1cf4a5608bc6b32620df9444d0ae0f0
Todd Gill <tgill@redhat.com>
2023-04-19 10:47:45 -0400

    add a script to setup the kubernetes dashboard
    Signed-off-by: Todd Gill <tgill@redhat.com>

---
+++ setup_dashboard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+
+kubectl create serviceaccount -n kubernetes-dashboard admin-user
+kubectl create clusterrolebinding -n kubernetes-dashboard admin-user --clusterrole cluster-admin --serviceaccount=kubernetes-dashboard:admin-user
+
+token=$(kubectl -n kubernetes-dashboard create token admin-user)
+echo $token
+ech http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
+kubectl proxy
+
