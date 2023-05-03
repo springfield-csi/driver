401da6e85c5bba78b83e8d90ce03b51df95cbfdb
Todd Gill <tgill@redhat.com>
2023-05-03 08:24:25 -0400

    update the duration time for the security token
    Signed-off-by: Todd Gill <tgill@redhat.com>

--- setup_dashboard.sh
+++ setup_dashboard.sh
@@ -5,8 +5,8 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/a
 kubectl create serviceaccount -n kubernetes-dashboard admin-user
 kubectl create clusterrolebinding -n kubernetes-dashboard admin-user --clusterrole cluster-admin --serviceaccount=kubernetes-dashboard:admin-user
 
-token=$(kubectl -n kubernetes-dashboard create token admin-user)
+token=$(kubectl -n kubernetes-dashboard create token --duration=700h admin-user)
 echo $token
-ech http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
+echo http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 kubectl proxy
 
