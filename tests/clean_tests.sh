dfc7d1efe15416bd06fb42031594d4bb5bee4f5e
Todd Gill <tgill@redhat.com>
2023-05-25 09:39:35 -0400

    cleanup LVM, BTRFS MD and Stratis Pods and PVCs
    Signed-off-by: Todd Gill <tgill@redhat.com>

--- tests/clean_tests.sh
+++ tests/clean_tests.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -x
 
 export KUBECTL=../bin/kubectl
-#$KUBECTL delete pv  springfield-pvc-test
-$KUBECTL delete pod springfield-pvc-test 
-$KUBECTL delete pvc springfield-pvc-claim
+
+$KUBECTL delete pod springfield-pvc-lvm-test
+$KUBECTL delete pod springfield-pvc-btrfs-test
+$KUBECTL delete pod springfield-pvc-md-test
+$KUBECTL delete pod springfield-pvc-stratis-test
+
+$KUBECTL delete pvc springfield-pvc-lvm-claim
+$KUBECTL delete pvc springfield-pvc-btrfs-claim
+$KUBECTL delete pvc springfield-pvc-md-claim
+$KUBECTL delete pvc springfield-pvc-stratis-claim
 
